19c2b2041e4f67a8be9d9a881c27e63e0b3f4da5
Myla <mila.mishchenko@outlook.com>
2022-11-03 19:31:35 +0100

    Added WPF project

---
+++ SourceCode/WhoWorks/WhoWorks.WPF/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+using WhoWorks.WPF.ViewModels;
+using WhoWorks.WPF.Views;
+
+namespace WhoWorks.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static IHost? AppHost { get; private set; }
+        public App()
+        {
+            IHostBuilder hostBuilder = Host.CreateDefaultBuilder();
+
+            hostBuilder.ConfigureServices((IServiceCollection services) =>
+            {
+                services.AddSingleton<MainWindow>();
+                services.AddSingleton<MainWindowViewModel>();
+            });
+
+            AppHost = hostBuilder.Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs args)
+        {
+            await AppHost?.StartAsync();
+
+            var startWindow = AppHost.Services.GetRequiredService<MainWindow>();
+            startWindow.DataContext = AppHost.Services.GetRequiredService<MainWindowViewModel>();
+            startWindow.Show();
+
+            base.OnStartup(args);
+        }
+    }
+}
